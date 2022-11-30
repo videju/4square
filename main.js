2de784d323970a2395923331ecfb13b4d163c8e2
videju <77975858+videju@users.noreply.github.com>
2022-11-30 15:52:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,112 @@
+console.log("hello")
+const navMenu = document.getElementById("nav-menu"),
+toggleMenu = document.getElementById('toggle-menu'),
+closeMenu = document.getElementById('close-menu')
+
+toggleMenu.addEventListener("click", () =>{
+    navMenu.classList.toggle('show');
+})
+
+closeMenu.addEventListener("click", () =>{
+    navMenu.classList.remove('show');
+})
+
+
+
+
+//making navbar sticky
+header = document.querySelector(".header")
+
+window.onscroll = function(){
+    var top =  window.scrollY;
+    console.log(top);
+    if (top >= 10){
+        header.classList.add("sticky")
+    }else{
+        header.classList.remove("sticky")
+
+    }
+    //owl-carousel script
+    
+}
+
+var counter = 1 ;
+setInterval(function(){
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if(counter > 4){
+      counter = 1;
+    }
+  }, 5000);
+  //homesection//
+  var slides = document.querySelectorAll('.slide');
+  var btns = document.querySelectorAll('.btn');
+  let currentSlide = 1;
+
+
+  var manualNav = function(manual){
+    slides.forEach((slide) => {
+      slide.classList.remove('active');
+
+      btns.forEach((btn) => {
+        btn.classList.remove('active');
+      });
+    });
+
+    slides[manual].classList.add('active');
+    btns[manual].classList.add('active');
+  }
+
+  btns.forEach((btn, i) => {
+    btn.addEventListener("click", () => {
+      manualNav(i);
+      currentSlide = i;
+    });
+  });
+
+
+  var repeat = function(activeClass){
+    let active = document.getElementsByClassName('active');
+    let i = 1;
+
+    var repeater = () => {
+      setTimeout(function(){
+        [...active].forEach((activeSlide) => {
+          activeSlide.classList.remove('active');
+        });
+
+      slides[i].classList.add('active');
+      btns[i].classList.add('active');
+      i++;
+
+      if(slides.length == i){
+        i = 0;
+      }
+      if(i >= slides.length){
+        return;
+      }
+      repeater();
+    }, 10000);
+    }
+    repeater();
+  }
+  repeat();
+  
+  //login_section code
+
+const loginForm = document.querySelector("form.login");
+const signupForm = document.querySelector("form.signup");
+const loginBtn = document.querySelector("label.login");
+const signupBtn = document.querySelector("label.signup");
+const signupLink = document.querySelector(".signup-link a");
+
+signupBtn.onclick = (()=>{
+  loginForm.style.marginLeft = "-50%"
+})
+loginBtn.onclick = (()=>{
+  loginForm.style.marginLeft = "0%"
+})
+signupLink.onclick = (()=>{
+  signupBtn.click();
+    return false;
+});
